f23a25419334e5a86237abdde9d04508b5068745
Anton Maminov <anton.linux@gmail.com>
2012-08-13 23:21:32 +0300

    style finatra-flash for bootstrap alerts

--- torrent_files_analyzer.rb
+++ torrent_files_analyzer.rb
@@ -44,6 +44,20 @@ def get_torrent_from_url(url)
   return file
 end
 
+# http://twitter.github.com/bootstrap/javascript.html#alerts
+module Sinatra
+  module Flash
+    module Style
+      def styled_flash(key=:flash)
+        return "" if flash(key).empty?
+        id = (key == :flash ? "flash" : "flash_#{key}")
+        close = '<a class="close" data-dismiss="alert" href="#">×</a>'
+        messages = flash(key).collect {|message| "  <div class='alert alert-#{message[0]}'>#{close}\n #{message[1]}</div>\n"}
+        "<div id='#{id}'>\n" + messages.join + "</div>"
+      end
+    end
+  end
+end
 
 class TorrentFilesAnalyzer < Sinatra::Base
   set :sessions, true
